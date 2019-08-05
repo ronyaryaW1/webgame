128d4f015e77d459028c395db5697e5a7e33a803
ronyaryaW1 <bangwijay11@gmail.com>
2019-08-05 07:20:13 -0700

    firstCommit

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1565014623,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/balon-sheet0.png",
+		"images/karakter-sheet0.png",
+		"images/peluru-sheet0.png",
+		"images/letusan.png",
+		"images/backgroundbygreggman-sheet0.png",
+		"images/tali.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
